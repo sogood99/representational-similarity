0e07101099ad6032c5253c3c95c4a67923170c29
Onii Chan <46487694+sogood99@users.noreply.github.com>
2025-02-24 10:11:19 -0500

    Update lims

--- test_cka.py
+++ test_cka.py
@@ -13,15 +13,19 @@ import matplotlib.animation as animation
 from cka import CKA, CKA_derivative
 
 
+def center(Z):
+    return Z - Z.mean(axis=0)
+
+
 if __name__ == "__main__":
     fig, ax = plt.subplots()
 
     origin = np.array([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0]])
     Z = np.array([[-1, 1], [-1, -2], [1, 1], [1, -1], [2, 1]], dtype=np.float32)
     # center the data
-    Z = Z - Z.mean(axis=0)
+    Z = center(Z)
     plt.title("Frame 0: Centering")
-    y = np.array([0, 0, 1, 1, 1])
+    y = np.array([1, 0, 0, 1, 1])
     y_o = y
     # one hot encoding
     y = np.eye(np.max(y) + 1)[y]
@@ -37,29 +41,29 @@ if __name__ == "__main__":
         scale_units="xy",
         scale=1,
     )
-    # ax.xlim(-4, 4)
-    # ax.ylim(-4, 4)
-    ax.set_xlim(-10, 10)
-    ax.set_ylim(-10, 10)
+    ax.set_xlim(-4, 4)
+    ax.set_ylim(-4, 4)
+    # ax.set_xlim(-10, 10)
+    # ax.set_ylim(-10, 10)
 
     def update(frame):
         global Z, rd
 
-        if frame % 2 == 0:
+        if frame % 3 == 0:
             # centering
-            Z = Z - Z.mean(axis=0)
+            Z = center(Z)
             plt.title(f"Frame {frame}: Centering")
 
-        elif frame % 2 == 1:
+        elif frame % 3 == 1:
             plt.title(f"Frame {frame}: Left Derivative")
             _, (ld, rd) = CKA_derivative(Z, y)
 
             ld, rd = ld.cpu().numpy(), rd.cpu().numpy()
-            Z -= rd
+            Z += ld
         else:
             plt.title(f"Frame {frame}: Right Derivative")
 
-            Z += ld
+            Z -= rd
 
         # quiver.set_offsets(Z)
         quiver.set_UVC(Z[:, 0], Z[:, 1])
@@ -70,5 +74,5 @@ if __name__ == "__main__":
     # show the grid
     ax.grid(color="gray", linestyle="--", linewidth=0.5)
 
-    ani = animation.FuncAnimation(fig, update, frames=range(20), interval=100)
+    ani = animation.FuncAnimation(fig, update, frames=range(21), interval=50)
     plt.show()
